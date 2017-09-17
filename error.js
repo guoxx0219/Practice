6f247d52e2624626abbd368ebe303cf3e44192fd
guoxx0219 <31531225+guoxx0219@users.noreply.github.com>
2017-09-17 14:59:38 +0800

    Delete error.js

--- error.js
+++
@@ -1,9 +0,0 @@
-let info = document.getElementById('info');
-let t = setInterval(function(){
-	if (info.innerText > 0) {
-		info.innerText -= 1; 
-	}else{	
-		clearInterval(t);
-		location.replace('index.html');
-	}
-},1000);
